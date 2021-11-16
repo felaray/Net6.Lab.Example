9896869e626631da87f7d1a5b07f4ea04d3f1a4b
Felaray Ho <vivatn@msn.com>
2021-11-16 14:52:05 +0800

    加入ai coding test

---
+++ Net6.Lab.Example/Controllers/LoopController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net6.Lab.Example.Models;
+
+namespace Net6.Lab.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoopController : ControllerBase
+    {
+        public IActionResult Test()
+        {
+            var test = new Test();
+
+            foreach (var item in test.List)
+            {
+                foreach (var item2 in item.List)
+                {
+                    foreach(var item3 in item2.List)
+                    {
+
+                    }
+                }
+            }
+
+            return Ok();
+
+        }
+    }
+}
---
+++ Net6.Lab.Example/Models/Test.cs
@@ -0,0 +1,8 @@
+﻿namespace Net6.Lab.Example.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public List<Test> List { get; set; }    
+    }
+}
